26417fdb2e12fa78ee6dc6cf27538e7de0a14a04
Tien-Duc Nguyen <ducntien12@gmail.com>
2023-10-12 12:50:04 +0200

    Fix import dataset snippets error

--- server/src/main/java/com/ntd/unipassau/codeannotation/export/ExportModelMapper.java
+++ server/src/main/java/com/ntd/unipassau/codeannotation/export/ExportModelMapper.java
@@ -1,8 +1,7 @@
 package com.ntd.unipassau.codeannotation.export;
 
 import com.ntd.unipassau.codeannotation.domain.dataset.Snippet;
-import com.ntd.unipassau.codeannotation.domain.question.Question;
-import com.ntd.unipassau.codeannotation.export.model.QuestionDoc;
+import com.ntd.unipassau.codeannotation.domain.rater.SnippetRate;
 import com.ntd.unipassau.codeannotation.export.model.SnippetDoc;
 import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
@@ -23,21 +22,32 @@ public interface ExportModelMapper {
     @Mapping(target = "createdDate", ignore = true)
     @Mapping(target = "createdBy", ignore = true)
     @Mapping(target = "code", ignore = true)
-    @Mapping(target = "rate.comment", source = "comment")
-    @Mapping(target = "rate.value", source = "rate")
-    @Mapping(target = "rate.createdBy", source = "rater")
-    @Mapping(target = "rate.lastModifiedBy", source = "rater")
-    @Mapping(target = "rate.rater.id", source = "rater")
+    @Mapping(target = "rate", ignore = true)
     Snippet toSnippet(SnippetDoc snippet);
 
+    @Mapping(target = "snippet", ignore = true)
+    @Mapping(target = "lastModifiedDate", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdDate", ignore = true)
+    @Mapping(target = "comment", source = "comment")
+    @Mapping(target = "value", source = "rate")
+    @Mapping(target = "createdBy", source = "rater")
+    @Mapping(target = "lastModifiedBy", source = "rater")
+    @Mapping(target = "rater.id", source = "rater")
+    SnippetRate toSnippetRate(SnippetDoc snippet);
+
     @AfterMapping
-    default void afterToSnippet(@MappingTarget Snippet snippet) {
+    default void afterToSnippet(SnippetDoc snippetDoc, @MappingTarget Snippet snippet) {
         // Ensure bi-directional relation
         snippet.getQuestions().forEach(q -> {
             q.setSnippet(snippet);
         });
-        if (snippet.getRate() != null) {
-            snippet.getRate().setSnippet(snippet);
+        if (snippetDoc.getRate() != null) {
+            SnippetRate snippetRate = toSnippetRate(snippetDoc);
+            snippet.setRate(snippetRate);
+            snippetRate.setSnippet(snippet);
+            if (null == snippetDoc.getRater())
+                snippetRate.setRater(null);
         }
     }
 }
