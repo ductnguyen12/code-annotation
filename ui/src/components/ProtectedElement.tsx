a1fcc43713bd1d0bac149b45789e2c367cf544dd
Tien-Duc Nguyen <ducntien12@gmail.com>
2023-10-26 10:34:57 +0200

    Reduce number of calling /me API

--- ui/src/components/ProtectedElement.tsx
+++ ui/src/components/ProtectedElement.tsx
@@ -1,6 +1,7 @@
 import { FC, ReactElement } from "react";
 import { Navigate } from "react-router-dom";
-import { useAuthentication } from "../hooks/auth";
+import { useAppSelector } from "../app/hooks";
+import { selectAuthState } from "../slices/authSlice";
 
 type ProtectedRouteProps = {
   children: ReactElement,
@@ -11,7 +12,7 @@ const ProtectedElement: FC<ProtectedRouteProps> = ({
   children,
   hidden = false,
 }): ReactElement => {
-  const { authenticated } = useAuthentication();
+  const { authenticated } = useAppSelector(selectAuthState);
   if (!authenticated) {
     if (!hidden)
       return (<Navigate to="/sign-in" replace />);
