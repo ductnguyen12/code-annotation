64a4d40ad3300d61792be04608895235da8528c5
Tien-Duc Nguyen <ducntien12@gmail.com>
2024-03-22 16:43:52 +0100

    Show details of constraint violations error

--- ui/src/util/error-util.ts
+++ ui/src/util/error-util.ts
@@ -25,6 +25,10 @@ export const getErrorMessage = (error: any, message?: string) => {
       errorMsg = error.response.data.fieldErrors
         .map((fieldError: any) => fieldError.message)
         .join(' and ');
+    } else if (error.response.data.violations && error.response.data.violations.length > 0) {
+      errorMsg = error.response.data.violations
+        .map((fieldError: any) => fieldError.message)
+        .join(' and ');
     }
   }
   return errorMsg;
