26ad869da94891f8d166ada6cf97199b14c8a6f1
Tien-Duc Nguyen <ducntien12@gmail.com>
2024-08-19 14:29:55 +0200

    Record completed time based on raters actions

--- server/src/main/java/com/ntd/unipassau/codeannotation/service/RaterActionService.java
+++ server/src/main/java/com/ntd/unipassau/codeannotation/service/RaterActionService.java
@@ -10,10 +10,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Calendar;
 import java.util.Optional;
 
 @Service
 public class RaterActionService {
+    private final static String SUBMIT_ACTION = "SUBMIT";
+    private final static String UPDATE_ACTION_PREFIX = "SET_";
+
     private final RaterActionMapper raMapper;
     private final RaterActionRepository raRepository;
     private final RaterDatasetRepository rdRepository;
@@ -35,11 +39,23 @@ public class RaterActionService {
         rdId.setDatasetId(actionVM.getDatasetId());
 
         return rdRepository.findById(rdId)
+                .map(raterDataset -> saveStartOrCompleteTime(actionVM.getAction(), raterDataset))
                 .map(raterDataset -> {
                     RaterAction raterAction = raMapper.toRaterAction(actionVM);
                     raterAction.setRaterDataset(raterDataset);
                     return raRepository.save(raterAction);
                 })
                 .map(raMapper::toRaterActionVM);
+    }
+
+    private RaterDataset saveStartOrCompleteTime(String action, RaterDataset raterDataset) {
+        if (SUBMIT_ACTION.equalsIgnoreCase(action)) {
+            raterDataset.setCompletedAt(Calendar.getInstance().getTime());
+        } else if (action.startsWith(UPDATE_ACTION_PREFIX)
+                && raterDataset.getCompletedAt() != null) {
+            raterDataset.setCompletedAt(Calendar.getInstance().getTime());
+        }
+        rdRepository.updateCompletedAtById(raterDataset.getId(), raterDataset.getCompletedAt());
+        return raterDataset;
     }
 }
